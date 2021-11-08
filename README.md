9e4244a6395be8ad3f449becb3032f501d591feb
Hritik Agarwal <hritikagarwal456@gmail.com>
2021-11-09 01:24:40 +0530

    created load balancer

---
+++ README.md
@@ -0,0 +1,23 @@
+## What is load balancer?
+
+Load Balancer is a mechanism which is used to distribute incoming traffic among the servers.
+
+## Why are load balancers used?
+
+These are used to improve the performance of the system and to ensure that resources are optimally utilized to produce maximum throughput.
+
+## How load balancers work?
+
+Load Balancers uses a particular algorithm to choose a server among all to send the request to. After that when any http request comes to load balancer they proxy it to the choosen server.
+
+## Load Balaning Algorithms
+
+3 commonly used alogirthms are -
+
+1. Round Robin => Load balancer choose server in a round robin fashion i.e. 1 -> 2 -> ... n-1 -> n -> 1 -> 2 ...
+2. Least Connection => Load balancer picks up the server which is handling least amount of traffic
+3. IP Hash => In this server is choosen on the basis of client IP
+
+## How it is implemented in the code?
+
+The load balancer is a process that takes in the HTTP requests and forwards these HTTP requests to one of the server among collection of servers available.
---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const proxy = require("express-http-proxy");
+const path = require('path');
+const ejs = require('ejs'); 
+
+const NUMBER_OF_SERVERS = 5;
+const PORT = 3000 || process.env.PORT;
+
+class Server{
+  constructor(active, port) {
+    this.active = active;
+    this.port = port;
+  }
+}
+
+let servers = [];
+
+function createServers(port) {
+  let app = express();
+  app.set("view engine", "ejs");
+  app.get('/', (req, res) => {
+    res.render('index.ejs', {
+      serverNumber: port - PORT,
+      serversState: servers.map(s => {
+        if (s.active) return "green";
+        else return "red";
+      })
+    });
+  });
+  app.listen(port);
+  let newServer = new Server(true, port);
+  servers.push(newServer);
+}
+
+let currServer = 0;
+
+// Load Balancer: Round Robin Algorithm
+function loadBalancerRoundRobin() {
+  let passes = 0;
+  while (true) {
+    if (passes == NUMBER_OF_SERVERS || currServer<0 || currServer>=NUMBER_OF_SERVERS) {
+      currServer = 0;
+      for (let i = 0; i < NUMBER_OF_SERVERS; i++) servers[i].active = true;
+    }
+    const port = PORT + 1 + currServer;
+    if (servers[currServer].active) {
+      const server = `http://localhost:${port}`;
+      currServer = (currServer + 1) % NUMBER_OF_SERVERS;
+      return server;
+    }
+    currServer = (currServer + 1) % NUMBER_OF_SERVERS;
+    passes++;
+  }
+}
+
+// Main server where the request will be routed through different servers using load balancer algorithm
+const app = require('./app');
+app.get("/", proxy(loadBalancerRoundRobin));
+app.post('/', (req, res) => {
+  let server = req.body.server-1;
+  servers[server].active = !servers[server].active;
+  currServer--;
+  res.redirect('/');
+})
+app.listen(PORT, () => console.log(`server is running on port %{PORT}`));
+
+// creating all the instances of different servers
+for (let i = PORT+1; i <= PORT + 1 + NUMBER_OF_SERVERS - 1; i++) createServers(i);
